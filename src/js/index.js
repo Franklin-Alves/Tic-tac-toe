81866dd2c28d61dbae1401f6ec2c7946a3c7800c
Franklin <antoniofranklin576@gmail.com>
2024-01-13 22:01:06 -0300

    Seleção de marcação única

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,39 @@
 let btns = document.querySelectorAll(".btn")
-let markerSelected = 
+let chooseBtn = document.querySelectorAll(".btns-selections .choose-btn")
 
+let firstClick = true
+const options = [""]
 
-btns.forEach((btn, ind)=> {
-    
-    btn.addEventListener('click', () => {
+chooseBtn.forEach((choice, ind) => {
+    choice.addEventListener('click', () => {
+        
+        if(firstClick){
 
-        btn.classList.add("markerX")
-        btn.innerHTML = "x"
+            let resultChoice = choice.getAttribute("class").substring(11)
+            let contentChoice = choice.textContent
+
+            console.log(relative)
+
+            markerChoose(resultChoice, contentChoice)
+        }
+
+        firstClick = false
     })
-    
 })
+
+function markerChoose (res, cont) {
+    btns.forEach((btn, ind)=> {
+        
+        btn.addEventListener('click', () => {
+
+            btn.classList.add(res)
+            btn.innerHTML = cont
+            opponentPlayer()
+        })
+        
+    })
+}
+
+function opponentPlayer(){
+
+}
